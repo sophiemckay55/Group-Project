a2f73afefae7bd8daca8f0112d2544f0523bb72e
sophiemckay55 <125925894+sophiemckay55@users.noreply.github.com>
2023-02-27 14:59:12 +0100

    Add files via upload

---
+++ Browsing.php
@@ -0,0 +1,174 @@
+<html>
+	<head>
+		<title>Rate a Restaurant</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap-styles.css" > 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 	
+	
+	</head>
+	
+	
+	<body id='myPage'>
+	
+	<!-- Header -->
+		<div class="jumbotron text-center">
+		<h1 style='color:crimson;'>Rate a Restaurant</h1> 
+		<p>Find your next great meal</p> 
+		</div>
+	
+	<!-- Filter -->
+	<div class="text-center">
+	<h3 style= 'color:crimson;'>Filter by: 5 star establishments</h3>
+	</div>
+	
+	<!-- Restaurant Example -->
+	<div id="Restaurant" class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>  Best Burgers <span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<h4>Casual brewpub that sells burgers, sandwiches, and other down-home favorites. </h4><br>
+					<h5>Jack Kim </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<p>Best Burgers is a hidden gem in the heart of the city! Their burgers are hands down the best I've ever had - juicy, flavorful, and cooked to perfection. The beer selection is great too, and the casual atmosphere is perfect for a laid-back night out with friends. Highly recommend the bacon cheeseburger and sweet potato fries!</p>
+					<h5>Jennifer Thomas </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<p>I recently visited Best Burgers and was blown away by the quality of their food. From the juicy burgers to the crispy chicken sandwiches, everything was delicious and reasonably priced. The staff were also very friendly and accommodating, making for a great overall experience. I'll definitely be back soon!</p>
+					<h5>Billy Blake </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<p>As a burger enthusiast, I can confidently say that Best Burgers lives up to its name. The burgers are made with high-quality ingredients and cooked just right, with a nice sear on the outside and a juicy, pink center. The toppings are also creative and delicious - I particularly enjoyed the blue cheese and bacon combo. The only downside is that the place can get pretty busy during peak hours, but it's well worth the wait!</p>
+
+				</div>
+				<div class="col-sm-4">
+				</div>
+			</div>
+		</div>
+
+	<!-- Restaurant Example -->
+	<div id="Restaurant" class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>  Pizza Paradise <span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<h4>Family owned pizzeria who's original owner hailed all the way from Naples, Italy. </h4><br>
+					<h5>Pete Blanker </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Pizza Paradise is hands down the best pizza joint in town! The crust is crispy, the sauce is tangy, and the toppings are always fresh and flavorful. You can tell that the owners take pride in their craft - they hail from Naples, after all! The service is also great, with friendly staff who are always happy to help. I highly recommend the margherita pizza - it's a classic for a reason!</p>
+					<h5>John Jameson </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>If you're looking for authentic Italian pizza, look no further than Pizza Paradise. The crust is thin and crispy, just like it should be, and the toppings are top-notch. I especially love the prosciutto and arugula pizza - it's the perfect balance of salty and fresh. The prices are also very reasonable for the quality of food you get. This is a must-visit for any pizza lover!</p>
+					<h5>Crystal Smith </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Pizza Paradise is a family-owned gem that truly embodies the spirit of Naples. The atmosphere is cozy and welcoming, and the pizzas are simply delicious. You can tell that the owners put a lot of love and care into every pie they make. I particularly love the sausage and mushroom pizza - the flavors are out of this world. If you're looking for a great pizza place with a personal touch, Pizza Paradise is the spot for you!</p>
+
+				</div>
+				<div class="col-sm-4">
+				</div>
+			</div>
+		</div>
+
+	<!-- Restaurant Example -->
+	<div id="Restaurant" class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>Sandwich Sanctuary <span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<h4>New American Bistro that offers build-your-own sandwiches at a reasonable price.</h4><br>
+					<h5>Liam Nguyen </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Sandwich Sanctuary is my new go-to spot for lunch! The build-your-own sandwich concept is so fun and allows you to customize your sandwich exactly how you like it. The ingredients are fresh and high-quality, and the prices are very reasonable. I also appreciate that they offer gluten-free and vegan options. Highly recommend the roast beef with horseradish mayo!</p>
+					<h5>Maya Patel </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>I recently visited Sandwich Sanctuary and was blown away by the quality of their sandwiches. The bread is freshly baked and the fillings are generous - I could barely finish my sandwich! The staff are also very friendly and helpful, and the atmosphere is cozy and inviting. The build-your-own concept is a great way to try new combinations, but they also have plenty of delicious pre-made options. I'll definitely be back!</p>
+					<h5>Isabella Russo </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Sandwich Sanctuary is a hidden gem that I'm so glad I discovered. The sandwiches are huge and filling, but also very reasonably priced. I love the variety of breads and fillings available, and the sauces and spreads really take the sandwiches to the next level. I also appreciate that they use eco-friendly packaging - it's great to see a restaurant that cares about the environment. Highly recommend the turkey and avocado sandwich!</p>
+				</div>
+				<div class="col-sm-4">
+				</div>
+			</div>
+		</div>
+
+	<!-- Restaurant Example -->
+	<div id="Restaurant" class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>Nifty Noodles <span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<h4>Classic chain with a wide menu of different noodles and pastas, from spaghetti bolognese to lo mein. </h4><br>
+					<h5>Lillian Rosier </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Nifty Noodles is a classic chain that never disappoints. The menu is huge, with so many different noodle and pasta dishes to choose from. I love the spaghetti bolognese - it's simple but so flavorful. The portions are also very generous, so you definitely get your money's worth. The atmosphere is cozy and inviting, making it the perfect spot for a casual dinner with friends or family.</p>
+					<h5>Alex Freeburg </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>If you're a fan of noodles and pasta, you need to check out Nifty Noodles. The menu is so extensive that you'll have a hard time deciding what to order! The dishes are all well-executed and delicious, with a variety of flavors and textures. The staff are also very friendly and accommodating - they always make sure you have everything you need. I highly recommend the pad thai - it's one of the best I've had!</p>
+					<h5>Joanne Petersburg </h5>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span>
+					<span class="glyphicon glyphicon-star logo"></span><br>
+					<p>Nifty Noodles is a great chain for a quick and tasty meal. The prices are very reasonable for the quality of food you get, and the portions are generous. I love the variety of sauces and toppings available - you can really customize your dish to your liking. The lo mein is a personal favorite of mine, but you can't go wrong with anything on the menu. The ambiance is also casual and laid-back, making it a great spot for a casual meal.</p>
+				</div>
+				<div class="col-sm-4">
+				</div>
+			</div>
+		</div>
+
