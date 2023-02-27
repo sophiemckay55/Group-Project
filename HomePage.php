7d0526f2f98287b187299cd686efbebbe915c897
sophiemckay55 <125925894+sophiemckay55@users.noreply.github.com>
2023-02-27 19:00:24 +0100

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>Home Page</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap-styles.css" > 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 	
+	
+	</head>
+	
+	<!-- Navbar -->
+	<nav class="navbar navbar-default">
+		  <div class="container">
+			<div class="navbar-header">
+		   	<a class="navbar-brand" href="#myPage"></a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a href="#login">LOG-IN</a></li>
+				<li><a href="#browse">BROWSE</a></li>
+				<li><a href="#reviews">REVIEWS</a></li>
+				<li><a href="#myratings">MY RATINGS</a></li>
+				<li><a href="#home">HOME</a></li>
+			  </ul>
+			</div>
+	 	 </div>
+		</nav>
+	
+	<!-- Header -->
+		<div class="jumbotron text-center">
+		<h1 style='color:crimson;'>Rate a Restaurant</h1> 
+		<p>Find your next great meal</p> 
+		</div>
+	
+	
+	<!-- Pictures -->
+	<div class="text-center">
+	<h2> <img src='spaghetti.jpeg'></img> <img src='pancakes.jpeg'></img> <img src='salad.jpeg'></img> <img src='chicken.jpeg'></img>
+	<br> <br> <br>
+	
+	
+	
+	
+	
+	
+	<!-- Log In Button -->
+	<form action="file:///Applications/MAMP/htdocs/GroupProject/LogIn.php">
+	<div class="text-center">
+    <button type="submit">Log-In</button>
+	</div>
+    </form>
+	
+	<!-- New User Button -->
+	<form action="file:///Applications/MAMP/htdocs/GroupProject/LogIn.php">
+	<div class="text-center">
+    <button type="submit">Register</button>
+	</div>
+    </form>
+    
+	
