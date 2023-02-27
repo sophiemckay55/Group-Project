197216a39d844e40f3b92337424afd190217902f
sophiemckay55 <125925894+sophiemckay55@users.noreply.github.com>
2023-02-27 11:52:00 +0100

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Rate a Restaurant</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap-styles.css" > 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 	
+	
+	</head>
+	
+	
+	<!-- Header -->
+		<div class="jumbotron text-center">
+		<h1 style='color:crimson;'>Rate a Restaurant</h1> 
+		<p>Find your next great meal</p> 
+		</div>
+	
+	<!-- Log In Button -->
+	<form action="file:///Applications/MAMP/htdocs/GroupProject/LogIn.php">
+	<div class="text-center">
+    <button type="submit">Log-In</button>
+	</div>
+    </form>
+	
+	
